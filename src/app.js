b40693be5abc8c6eb759245ac858d9e9a92f11bb
snehil <snehil@snehils-MacBook-Air.local>
2025-06-16 01:17:54 +0530

    working on multiple http methos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-const express=require("express")
-const app= express()
-app.use("/test",(req,res)=>{
-   res.send("Hello from server")
-})
-app.use("/",(req,res)=>{
-   res.send("Dashboard")
-})
-app.listen(7777,()=>{
-    console.log("Server is successfully started")
-})
+const express = require("express");
+const app = express();
+
+//this will handle only get method
+app.get("/hello", (req, res) => {
+  res.send("hello get");
+});
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "snehil", lastName: "kumar" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("data succesfully saved");
+});
+//this will matchall the http method API call to /hello
+app.use("/hello", (req, res) => {
+  res.send("hello all");
+});
+
+app.listen(7777, () => {
+  console.log("Server is successfully started");
+});
