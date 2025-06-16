64d66d88946b02ae1bbae3a2262c58d62437f9eb
snehil <snehil@snehils-MacBook-Air.local>
2025-06-16 19:05:27 +0530

    using try catch block to handle error

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
-const {auth} = require("../middleware/auth");
-app.use("/admin", auth);
+const { auth } = require("../middleware/auth");
+app.use("/", (req, res, next) => {
+  if (err) {
+    console.log("error found");
+  } else {
+    console.log("nothing found");
+    next(err);
+  }
+});
 app.get("/admin/getAllData", (req, res, next) => {
-  res.send("all user data");
+  try {
+    console.log("hello boy");
+    res.send("all user data");
+  } catch (err) {
+    res.send(400).send("400 error occured");
+  }
 });
 app.get("/admin/deleteone", (req, res, next) => {
   res.send("delet one ");
