5525095ba6f910fb0d2274b20e1309772105e224
snehil <snehil@snehils-MacBook-Air.local>
2025-06-16 16:13:01 +0530

    creating nesting request handler

--- src/app.js
+++ src/app.js
@@ -1,22 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.get("/abc/:userId/:name", [
-  (req, res, next) => {
-    console.log("First route handler");
-    //  res.send(`Response !!`);
-    next();
-  },
-  (req, res, next) => {
-    console.log("second route handler");
-    next();
-    //  res.send("2nd response");
-  },
-  (req, res, next) => {
-    console.log("third route handler");
-    res.send("3nd response");
-  },
-]);
+app.use("/", (req, res, next) => {
+  console.log("Dashboard");
+ next()
+});
+app.use("/abc", (req, res, next) => {
+  console.log("abc");
+  next();
+});
+app.use("/user", (req, res, next) => {
+   console.log("user");
+   res.send("user")
+ });
 
 app.listen(7777, () => {
   console.log("Server is successfully started");
