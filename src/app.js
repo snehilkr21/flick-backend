06d63f98570d8a71887b246b5d3fe7ba6d848825
snehil <snehil@snehils-MacBook-Air.local>
2025-06-16 14:29:39 +0530

    Playing with mutiple route handler

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/abc", (req, res) => {
-  console.log(req.query);
-  res.send(`hello`);
-});
+app.get("/abc/:userId/:name", [
+  (req, res, next) => {
+    console.log("First route handler");
+    //  res.send(`Response !!`);
+    next();
+  },
+  (req, res, next) => {
+    console.log("second route handler");
+    next();
+    //  res.send("2nd response");
+  },
+  (req, res, next) => {
+    console.log("third route handler");
+    res.send("3nd response");
+  },
+]);
 
 app.listen(7777, () => {
   console.log("Server is successfully started");
