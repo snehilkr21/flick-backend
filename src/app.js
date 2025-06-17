43d0e61f9320bca96d3b1ca11f8661e51fcc9cd0
snehil <snehil@snehils-MacBook-Air.local>
2025-06-17 23:34:13 +0530

    get all user and particular user

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const connectDB=require("./config/database")
-const UserModel= require("./modles/user")
+const connectDB = require("./config/database");
+const UserModel = require("./modles/user");
 
-app.use(express.json())
+app.use(express.json());
 
 connectDB()
   .then(() => {
@@ -15,14 +15,37 @@ connectDB()
   .catch((err) => {
     console.error("Databse cannot be connected");
   });
-app.post("/signup", async (req,res)=>{
+app.post("/signup", async (req, res) => {
   // i  am creating a new instance of a user model using the data which we got from the client
-  const userObj= new UserModel(req.body)
-  try{
-    await userObj.save()
-    res.send("User added Successfully")
-  }catch(err){
-    res.send(400).send("Error while saving the User" + err.message)
+  const userObj = new UserModel(req.body);
+  try {
+    await userObj.save();
+    res.send("User added Successfully");
+  } catch (err) {
+    res.status(400).send("Error while saving the User" + err.message);
   }
- 
-})
+});
+
+//to get user of certain on basis of firstName
+app.get("/user", async (req, res) => {
+  const firstName = req.body.firstName;
+
+  try {
+    const data = await UserModel.find({ firstName: firstName });
+    if (data.length) res.send(data);
+    else res.status(404).send("User not found");
+  } catch (err) {
+    res.status(400).send("Cannot find User ", err.message);
+  }
+});
+
+//to get all User
+app.get("/feed", async (req, res) => {
+  try {
+    const data = await UserModel.find({});
+    if (data.length) res.send(data);
+    else res.status(404).send("Not Found");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
