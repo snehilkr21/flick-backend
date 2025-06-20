890bbde31f1e99c9da42401e82fd1f7ab851d899
snehil <snehil@snehils-MacBook-Air.local>
2025-06-20 20:53:00 +0530

    /profile/edit

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
   } catch (err) {
     res.status(400).send("Error ", err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+    const logedInUser = req.user;
+    Object.keys(req.body).forEach((key) => (logedInUser[key] = req.body[key]));
+    await logedInUser.save();
+    res.json({
+      message: `${logedInUser.firstName} your profile updated`,
+      data: logedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("Error " + err.message);
   }
 });
 
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -11,7 +11,23 @@ const validateSignUpData = (req) => {
     throw new Error("Enter a strong password!");
   }
 };
-
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+  return isEditAllowed;
+};
 module.exports = {
   validateSignUpData,
+  validateEditProfileData,
 };
