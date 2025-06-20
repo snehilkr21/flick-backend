0cd7df92e5b6af45d0f0799bc580c6f5a45d0e50
snehil <snehil@snehils-MacBook-Air.local>
2025-06-20 23:18:00 +0530

    /profile/password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
+const UserModel = require("../modles/user");
 const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
+var validator = require("validator");
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
@@ -18,11 +21,37 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
       throw new Error("Invalid Edit Request");
     }
     const logedInUser = req.user;
-    Object.keys(req.body).forEach((key) => (logedInUser[key] = req.body[key]));
-    await logedInUser.save();
+    await UserModel.findByIdAndUpdate({ _id: logedInUser._id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
     res.json({
       message: `${logedInUser.firstName} your profile updated`,
       data: logedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("Error " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { newPassword } = req.body;
+    if (!validator.isStrongPassword(newPassword)) {
+      throw new Error("Enter a strong password");
+    }
+
+    const bcryptNewPassword = await bcrypt.hash(newPassword, 10);
+    await UserModel.findByIdAndUpdate(
+      { _id: req.user._id },
+      { password: bcryptNewPassword },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.json({
+      message: `Hey your password is updated`,
     });
   } catch (err) {
     res.status(400).send("Error " + err.message);
