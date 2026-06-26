1afc3fe4ef6d9b3ef5901f0ba1cd36170836a36d
Snehil <snehil@192.168.1.6>
2026-06-26 20:06:18 +0530

    cors issue

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -12,7 +12,10 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initilizeSocket = (server) =>{
     const io = socket(server, {
       cors : {
-        origin : "http://localhost:5173"
+        origin: [
+            "http://localhost:5173",
+            "https://flickworld.online"
+        ],
       }
     })
     
